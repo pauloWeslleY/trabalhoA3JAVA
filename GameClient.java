2b5968e9f35dfc481283f16a778dcce3af212e8d
Weslley Lima <46795065+pauloWeslleY@users.noreply.github.com>
2024-06-07 11:42:40 -0300

    Add files via upload

--- GameClient.java
+++ GameClient.java
@@ -1,13 +1,17 @@
+package jogo1;
 import java.io.*;
 import java.net.*;
 
 public class GameClient {
-  private static final String SERVER_ADDRESS = "localhost";
   private static final int SERVER_PORT = 12345;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader userIp = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Digite o IP:");
+    String IP = userIp.readLine();
+
     try {
-      Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+      Socket socket = new Socket(IP, SERVER_PORT);
       BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
       PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
       BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
